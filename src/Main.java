48c71783bd31de02ed3499469e255f89938ffccd
127DReger <127dreger@frhsd.com>
2025-09-10 08:10:30 -0400

    Updated welcome message, updated pd1 class and added level question, added array list start

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Welcome To The New School Year!");
+        System.out.println("For the following questions please use a number that correlates to the difficulty of your class (1-Regular, 2-Honors, 3-AP)");
         System.out.println("");
+
+        ArrayList<Integer> numbers = new ArrayList<>();
 
         Scanner sc = new Scanner(System.in);
         {
-        System.out.println("Please enter your Period 1 class");
+        System.out.println("Please enter your Period 1 class: ");
+        String name = scanner.next.Line();
+        System.out.println("Please enter the difficulty of the class: ");
 
         }
        }
