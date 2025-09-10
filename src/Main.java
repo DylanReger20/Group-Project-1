555a53785d9dad2e1c969b38f816b9b1db527d7d
127HHernandez <127Hhernandez@frhsd.com>
2025-09-10 08:17:39 -0400

    ArrayList question for difficulty of the class

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Welcome To The New School Year!");
         System.out.println("For the following questions please use a number that correlates to the difficulty of your class (1-Regular, 2-Honors, 3-AP)");
         System.out.println("");
 
-        ArrayList<Integer> numbers = new ArrayList<>();
+        ArrayList<Integer> difficultyLevel = new ArrayList<>();
+        difficultyLevel.add(Regular);
+        difficultyLevel.add(Honors);
+        difficultyLevel.add(AP);
+
 
         Scanner sc = new Scanner(System.in);
         {
         System.out.println("Please enter your Period 1 class: ");
-        String name = scanner.next.Line();
+        String name = sc.nextLine();
         System.out.println("Please enter the difficulty of the class: ");
+        Integer difficulty = sc.nextInt();
+
 
         }
        }
