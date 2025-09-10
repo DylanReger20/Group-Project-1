9559ea60ac3608dcb79998e6f14092185b8e0718
127DReger <127dreger@frhsd.com>
2025-09-10 08:26:29 -0400

    Finished period 1 questions and did letterGrade arraylist

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,40 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Welcome To The New School Year!");
-        System.out.println("For the following questions please use a number that correlates to the difficulty of your class (1-Regular, 2-Honors, 3-AP)");
+        System.out.println("For the following questions please use a number that correlates to the difficulty of your class(Regular,Honors,AP)");
         System.out.println("");
 
-        ArrayList<Integer> difficultyLevel = new ArrayList<>();
-        difficultyLevel.add(Regular);
-        difficultyLevel.add(Honors);
-        difficultyLevel.add(AP);
+        ArrayList<String> difficultyLevel = new ArrayList<>();
+        difficultyLevel.add("Regular");
+        difficultyLevel.add("Honors");
+        difficultyLevel.add("AP");
+
+        ArrayList<String> letterGrade = new ArrayList<>();
+        letterGrade.add("A+");
+        letterGrade.add("A");
+        letterGrade.add("A-");
+        letterGrade.add("B+");
+        letterGrade.add("B");
+        letterGrade.add("B-");
+        letterGrade.add("C+");
+        letterGrade.add("C");
+        letterGrade.add("C-");
+        letterGrade.add("D+");
+        letterGrade.add("D");
+        letterGrade.add("D-");
+        letterGrade.add("E");
 
 
         Scanner sc = new Scanner(System.in);
         {
+            //Period 1
         System.out.println("Please enter your Period 1 class: ");
         String name = sc.nextLine();
         System.out.println("Please enter the difficulty of the class: ");
         Integer difficulty = sc.nextInt();
+        System.out.println("Now please enter your desired grade for this class: ");
+        String grade = sc.nextLine();
+            //Period 2
 
 
         }
